11ade26d2763d7008bc4cb17f4d84cee7f81780e
deepanakash-001 <deepanakash63@gmail.com>
2025-04-09 15:20:16 +0530

    Created Dept class

---
+++ GitDemo/dept.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class dept
+    {
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+    }
+}
